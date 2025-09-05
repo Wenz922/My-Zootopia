a4d3846e9159fac20cc4b17ded0fad96eee2e00a
Wenzheng Cai <wenzhengcai@Wenzhengs-Air.fritz.box>
2025-09-05 17:55:32 +0200

    adjusted the code to be more readable

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,35 +2,60 @@ import json
 
 JSON_FILE = 'animals_data.json'
 HTML_FILE = 'animals_template.html'
+NEW_HTML_FILE = 'animals.html'
 
 
-def load_data(file_path):
+def load_json_data(json_file_path):
     '''Loads a JSON file'''
-    with open(file_path, 'r') as handle:
+    with open(json_file_path, 'r') as handle:
         return json.load(handle)
 
 
-animals_data = load_data(JSON_FILE)
-with open(HTML_FILE, 'r') as file:
-    html_data = file.read()
-#print(html_data)
+def load_html_data(html_file_path):
+    '''Loads a HTML file'''
+    with open(html_file_path, 'r') as handle:
+        return handle.read()
 
-output = ''     # define an empty string
-for animal in animals_data:
-    # append information to each string
+
+def write_html_data(new_file_path, data):
+    '''Writes a HTML file'''
+    with open(new_file_path, 'w') as new_file:
+        new_file.write(data)
+
+
+def serialize_animal(animal_obj):
+    '''Serializes an animal html object'''
+    output = ''
     output += '<li class="cards__item">'
-    output += f'<div class="card__title">{animal["name"]}</div>\n'
+    output += f'<div class="card__title">{animal_obj["name"]}</div>\n'
     output += '<p class="card__text">'
-    output += f'<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
-    output += f'<strong>Location:</strong> {animal["locations"][0]}<br/>\n'
-    if animal['characteristics'].get('type'):
-        output += f'<strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
+    output += f'<strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}<br/>\n'
+    output += f'<strong>Location:</strong> {animal_obj["locations"][0]}<br/>\n'
+    if animal_obj['characteristics'].get('type'):
+        output += f'<strong>Type:</strong> {animal_obj["characteristics"]["type"]}<br/>\n'
     output += '</p>'
     output += '</li>'
-#print(output)
+    return output
 
-new_html_data = html_data.replace("__REPLACE_ANIMALS_INFO__", output)
-#print(new_html_data)
 
-with open('animals.html', 'w') as file:
-    file.write(new_html_data)
+def main():
+    '''Main function'''
+    animals_data = load_json_data(JSON_FILE)
+    #print(animals_data)
+    html_data = load_html_data(HTML_FILE)
+    #print(html_data)
+
+    output = ''     # define an empty string
+    for animal in animals_data:
+        output += serialize_animal(animal)
+    #print(output)
+
+    new_html_data = html_data.replace("__REPLACE_ANIMALS_INFO__", output)
+    #print(new_html_data)
+
+    write_html_data(NEW_HTML_FILE, new_html_data)
+
+
+if __name__ == '__main__':
+    main()
+
